e26bedd3403542094b9ebcd34a375e4444aa01cf
lleopard <lleopard.sa@everis.com>
2018-07-14 10:03:40 -0500

    feat(map-iss.map-iss.service): implementa modelo viewer

--- src/app/map-iss/map-iss.service.ts
+++ src/app/map-iss/map-iss.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '../../../node_modules/@angular/common/http';
-import { Observable } from '../../../node_modules/rxjs/Observable';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "../../../node_modules/@angular/common/http";
+import { Observable } from "../../../node_modules/rxjs/Observable";
+import { PositionISS } from "./models/position-iss";
+import { PositionISSViewer } from "./models/position-iss-viewer";
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MapIssService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getPosition(): Observable<Object>{
-    const api = 'http://api.open-notify.org/iss-now.json';
-    return this.http.get(api);
+  getPosition(): Observable<PositionISSViewer> {
+    const api = "http://api.open-notify.org/iss-now.json";
+    return this.http.get(api)
+    .map((position: PositionISS)=>{
+      return new PositionISSViewer(position);
+    });
   }
-
 }
